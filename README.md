8fab101b8b96c87c6d525ca5b6fcd75624c7d70a
Craig Ford <craig_ford@icloud.com>
2023-01-19 12:55:48 -0500

    re migrated

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # README
 
+Got rid of seed data
+
 This README would normally document whatever steps are necessary to get the
 application up and running.
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,29 +6,29 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-puts "🤠 Seeding users..."
+# puts "🤠 Seeding users..."
 
-u1 = User.create(username: "ann", password: "ann", address: "91 E River Rd", city: "Rumson", state: "NJ", phone: "7323205798", email: "aa62@aol.com")
-u2 = User.create(username: "jay", password: "jay", address: "1140 Ballantine Rd", city: "Middletown", state: "NJ", phone: "7326414496", email: "jldr@bkook.com")
-u3 = User.create(username: "paul", password: "paul", address: "77 Helena St", city: "Wall", state: "NJ", phone: "7326465112", email: "popper72@gmail.com")
-u4 = User.create(username: "craig", password: "craig", address: "83 Waterman Ave", city: "Fair Haven", state: "NJ", phone: "7326871212", email: "clong@gmail.com")
+# u1 = User.create(username: "ann", password: "ann", address: "91 E River Rd", city: "Rumson", state: "NJ", phone: "7323205798", email: "aa62@aol.com")
+# u2 = User.create(username: "jay", password: "jay", address: "1140 Ballantine Rd", city: "Middletown", state: "NJ", phone: "7326414496", email: "jldr@bkook.com")
+# u3 = User.create(username: "paul", password: "paul", address: "77 Helena St", city: "Wall", state: "NJ", phone: "7326465112", email: "popper72@gmail.com")
+# u4 = User.create(username: "craig", password: "craig", address: "83 Waterman Ave", city: "Fair Haven", state: "NJ", phone: "7326871212", email: "clong@gmail.com")
 
-puts "🤠 Seeding dealers..."
+# puts "🤠 Seeding dealers..."
 
-d1 = Dealer.create(name: "Unique Gold", sales_rep: "Al Colquitt", address: "613 Van Houten Ave", city: "Clifton", state: "NJ", phone: "2015426745", email: "jo1254@gmail.com")
-d2 = Dealer.create(name: "NJ Gold Spot", sales_rep: "Jeff Egle", address: "1726 Frank St", city: "Middletown", state: "NJ", phone: "7326712550", email: "jeegle@aol.com")
-d3 = Dealer.create(name: "The Gold Trader", sales_rep: "Tom Och", address: "3130 Hargrove Ln", city: "Asbury Park", state: "NJ", phone: "6467429825", email: "to1988@yahoo.com")
-d4 = Dealer.create(name: "State Gold Buyers", sales_rep: "Pete Fry", address: "2903 Stonewall Ave", city: "Red Bank", state: "NJ", phone: "90838306666", email: "pfry@buyers.com")
-d5 = Dealer.create(name: "Verona Gold Coins", sales_rep: "Pat Rook", address: "59 Bunker Hill", city: "Nutley", state: "NJ", phone: "2012647891", email: "rooker432@gmail.com")
-d6 = Dealer.create(name: "D & S Gold Exchange", sales_rep: "Jeff Bosh", address: "747 Palomino Ave", city: "Verona", state: "NJ", phone: "2019395612", email: "j_bosh@goldexchange.com")
+# d1 = Dealer.create(name: "Unique Gold", sales_rep: "Al Colquitt", address: "613 Van Houten Ave", city: "Clifton", state: "NJ", phone: "2015426745", email: "jo1254@gmail.com")
+# d2 = Dealer.create(name: "NJ Gold Spot", sales_rep: "Jeff Egle", address: "1726 Frank St", city: "Middletown", state: "NJ", phone: "7326712550", email: "jeegle@aol.com")
+# d3 = Dealer.create(name: "The Gold Trader", sales_rep: "Tom Och", address: "3130 Hargrove Ln", city: "Asbury Park", state: "NJ", phone: "6467429825", email: "to1988@yahoo.com")
+# d4 = Dealer.create(name: "State Gold Buyers", sales_rep: "Pete Fry", address: "2903 Stonewall Ave", city: "Red Bank", state: "NJ", phone: "90838306666", email: "pfry@buyers.com")
+# d5 = Dealer.create(name: "Verona Gold Coins", sales_rep: "Pat Rook", address: "59 Bunker Hill", city: "Nutley", state: "NJ", phone: "2012647891", email: "rooker432@gmail.com")
+# d6 = Dealer.create(name: "D & S Gold Exchange", sales_rep: "Jeff Bosh", address: "747 Palomino Ave", city: "Verona", state: "NJ", phone: "2019395612", email: "j_bosh@goldexchange.com")
 
-puts "🤠 Seeding transactions..."
+# puts "🤠 Seeding transactions..."
 
-Transaction.create(user_id: u2.id, dealer_id: d2.id, num_ounces: 3, price_per_ounce: 1750)
-Transaction.create(user_id: u3.id, dealer_id: d2.id, num_ounces: 1, price_per_ounce: 1500)
-Transaction.create(user_id: u3.id, dealer_id: d3.id, num_ounces: 2, price_per_ounce: 1800)
-Transaction.create(user_id: u3.id, dealer_id: d6.id, num_ounces: 9, price_per_ounce: 1111)
-Transaction.create(user_id: u4.id, dealer_id: d2.id, num_ounces: 6, price_per_ounce: 1111)
-Transaction.create(user_id: u4.id, dealer_id: d3.id, num_ounces: 6, price_per_ounce: 1111)
+# Transaction.create(user_id: u2.id, dealer_id: d2.id, num_ounces: 3, price_per_ounce: 1750)
+# Transaction.create(user_id: u3.id, dealer_id: d2.id, num_ounces: 1, price_per_ounce: 1500)
+# Transaction.create(user_id: u3.id, dealer_id: d3.id, num_ounces: 2, price_per_ounce: 1800)
+# Transaction.create(user_id: u3.id, dealer_id: d6.id, num_ounces: 9, price_per_ounce: 1111)
+# Transaction.create(user_id: u4.id, dealer_id: d2.id, num_ounces: 6, price_per_ounce: 1112)
+# Transaction.create(user_id: u4.id, dealer_id: d3.id, num_ounces: 6, price_per_ounce: 1114)
 
-puts "✅ Done seeding!"
+# puts "✅ Done seeding!"
